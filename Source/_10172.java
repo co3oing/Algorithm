0548d756733d929abf570353823f8a3bedff3348
co3oing <co3oing@naver.com>
2020-05-19 18:35:13 +0900

    Update _10172.java

--- Source/_10172.java
+++ Source/_10172.java
@@ -4,10 +4,10 @@ public class _10172 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("|\\_/|");
-        		System.out.println("|q p|   /}");
+        	System.out.println("|q p|   /}");
      		System.out.println("( 0 )\"\"\"\\");
-        		System.out.println("|\"^\"`    |");
-        		System.out.println("||_/=\\\\__|");
+        	System.out.println("|\"^\"`    |");
+        	System.out.println("||_/=\\\\__|");
 	}
 
 }
