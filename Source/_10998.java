576c3d065177c9501e950711ac0ad4989cf44d87
co3oing <co3oing@naver.com>
2020-05-23 23:32:33 +0900

    Bronze V yeah~

---
+++ Source/_10998.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class _10998{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int answer = 0;
+
+		answer = A * B;
+		System.out.println(answer);
+	}
+}
