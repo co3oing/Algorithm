b3cb8b86b18c3dfcb0ff9f62f4ab050171b742e0
co3oing <co3oing@naver.com>
2021-03-17 16:28:16 +0900

    N으로 표현

---
+++ 깊이 우선 탐색(DFS)/N으로 표현.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+func solution(_ N:Int, _ number:Int) -> Int {
+    var result: Int = -1
+    dfs(N, number, 0, 0, &result)
+    return result
+}
+
+func dfs(_ N:Int, _ number:Int, _ countN:Int, _ resultN:Int, _ result: inout Int) {
+    var calN = 0
+    
+    if countN > 8 {
+        return
+    }
+    
+    if number == resultN && (countN < result || result == -1) {
+        result = countN
+        return
+    }
+    
+    for i in 0..<8 {
+        calN = calN * 10 + N
+        dfs(N, number, countN + 1 + i, resultN + calN, &result)
+        dfs(N, number, countN + 1 + i, resultN - calN, &result)
+        dfs(N, number, countN + 1 + i, resultN * calN, &result)
+        dfs(N, number, countN + 1 + i, resultN / calN, &result)
+    }
+}
