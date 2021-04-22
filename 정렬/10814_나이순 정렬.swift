a53f83be7e9c2604e0a8c282e9e559b23d0096f9
co3oing <co3oing@naver.com>
2021-04-22 17:46:50 +0900

    10814

--- 정렬/10814_나이순 정렬.swift
+++ 정렬/10814_나이순 정렬.swift
@@ -1,8 +1,18 @@
-//
-//  10814_나이순 정렬.swift
-//  BOJ
-//
-//  Created by Lucia on 2021/04/22.
-//
-
 import Foundation
+
+let N = Int(readLine() ?? "") ?? 0
+
+if 1 <= N && N <= 100000 {
+    var person: [[String]] = []
+    
+    for _ in 0..<N {
+        let arr = (readLine() ?? "").split(separator: " ").map{String($0)}
+        person.append(arr)
+    }
+    
+    person.sort{Int($0[0])! < Int($1[0])!}
+
+    for person in person {
+        print("\(person[0]) \(person[1])")
+    }
+}
